cc5c1bc7859a93f2136675af08fa687a82be80b8
Acttion101 <pongponpan102@gmail.com>
2020-09-09 00:37:31 +0700

    edit -migration(user)

--- database/migrations/1599581777096_create_user_schema.js
+++ database/migrations/1599581777096_create_user_schema.js
@@ -6,8 +6,14 @@ const Schema = use('Schema')
 class CreateUserSchema extends Schema {
   up () {
     this.create('create_users', (table) => {
-      table.increments()
-      table.timestamps()
+      table.increments('user_id')
+      table.string('frist_name').notNullble()
+      table.string('last_name').notNullble()
+      table.string('age').notNullble()
+      table.string('username').notNullble().unique()
+      table.string('password').notNullble()
+      table.string('status')
+      table.timestsamps()
     })
   }
 
