80d9941c882458b2057df9d01e78d361a81301ef
ramita-kahapana <ramitakahapana@gmail.com>
2020-09-11 21:41:05 +0700

    edit-migration/store

---
+++ database/migrations/1599581816109_create_store_schema.js
@@ -0,0 +1,32 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CreateStoreSchema extends Schema {
+    up() {
+        this.create('stores', (table) => {
+            table.increments('store_id', 5)
+            table.string('store_name', 150).notNullable()
+            table.string('detail', 150).notNullable()
+            table.string('comment_review', 100).notNullable()
+            table.integer('user_id').notNullable().unsigned()
+            table.integer('admin_id').notNullable().unsigned()
+            table.foreign('user_id')
+                .references('users.user_id')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE')
+            table.foreign('admin_id')
+                .references('admins.admin_id')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE')
+            table.timestamps()
+        })
+    }
+
+    down() {
+        this.drop('stores')
+    }
+}
+
+module.exports = CreateStoreSchema
