ccd2a4c7b4eeca7303fe0e1b0d6803ec51b768d8
ramita-kahapana <ramitakahapana@gmail.com>
2020-09-12 17:02:08 +0700

    edit-store/util

--- util/StoreUtil.js
+++ util/StoreUtil.js
@@ -6,39 +6,29 @@ class StoreUtil {
 
     getAll(references) {
         const store = this._Store.query()
+        if (references) {
+            const extractedReferences = references.split(",");
+            extractedReferences.forEach(reference => {
+                store.with(reference)
 
-        return this._withReference(store, references)
-            .fetch()
+            });
+        }
+        return store.fetch()
     }
 
     getById(storeId, references) {
         const store = this._Store
             .query()
             .where('store_id', storeId)
-
-        return this._withReference(store, references)
-            .fetch()
-            .then(response => response.first())
-    }
-
-    async create(storeInstance, references) {
-        const { store_id } = await this._Store.create(storeInstance)
-        const store = this._Store
-            .query()
-            .where('store_id', store_id)
-
-        return this._withReference(store, references)
-            .fetch()
-            .then(response => response.first())
-    }
-
-    _withReference(instance, references) {
         if (references) {
-            const extractedReferences = references.split(",")
-            instance.with(extractedReferences)
+            const extractedReferences = references.split(",");
+            store.with(extractedReferences)
         }
-
-        return instance
+        return store
+            .fetch()
+            .then(response => response.first())
     }
+
+
 }
 module.exports = StoreUtil
