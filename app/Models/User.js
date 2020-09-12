0efcd142bf6d402e6be09f8527079cafb9f3eb2c
ramita-kahapana <ramitakahapana@gmail.com>
2020-09-12 17:01:47 +0700

    edit-user/model

--- app/Models/User.js
+++ app/Models/User.js
@@ -5,25 +5,22 @@ const Model = use('Model')
 const Hash = use('Hash')
 class User extends Model {
 
-  static boot () {
-    super.boot()
-    this.addHook('beforeSave', async (userInstance) => {
-      if (userInstance.dirty.password) {
-        userInstance.password = await Hash.make(userInstance.password)
-      }
-    })
-  }
-  static get primaryKey() {
-    return 'user_id'
-  }
-  static get createdAtColumn() {
-    return null;
-  }
-  static get updatedAtColumn() {
-    return null;
-  }
+    static boot() {
+        super.boot()
+        this.addHook('beforeSave', async(userInstance) => {
+            if (userInstance.dirty.password) {
+                userInstance.password = await Hash.make(userInstance.password)
+            }
+        })
+    }
+    static get primaryKey() {
+        return 'user_id'
+    }
 
- 
+    stores() {
+        return this.hasMany('App/Models/Store')
+    }
+
 }
 
 module.exports = User
