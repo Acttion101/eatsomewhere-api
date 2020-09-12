22123457b63c7377c25ba8363e93fae86cd844fb
ramita-kahapana <ramitakahapana@gmail.com>
2020-09-12 15:05:06 +0700

    new-store/controller

---
+++ app/Controllers/Http/StoreController.js
@@ -0,0 +1,73 @@
+'use strict'
+const Store = use("App/Models/Store")
+
+const Database = use('Database')
+const StoreValidator = require("../../../service/StoreValidator")
+const StoreUtil = require("../../../util/StoreUtil")
+
+function numberTypeParamValidator(number) {
+    if (Number.isNaN(parseInt(number)))
+        return { error: `param: ${number} is not supported, please use number type param instead.` }
+    return {}
+}
+
+class StoreController {
+    async index({ request }) {
+        const { references } = request.qs
+        const storeUtil = new StoreUtil(Store)
+        const store = await storeUtil.getAll(references)
+
+        return { status: 200, error: undefined, data: store }
+    }
+    async show({ request }) {
+        const { id } = request.params
+        const { references } = request.qs
+        const storeUtil = new StoreUtil(Store)
+        const store = storeUtil.getById(id, references)
+        return { status: 200, error: undefined, data: store || {} }
+
+    }
+    async store({ request }) {
+        const { store_name, detail, comment_review, user_id, admin_id } = request.body
+        const { references } = request.qs
+
+
+
+        const storeUtil = new StoreUtil(Store)
+        const store = await storeUtil.create({ store_name, detail, comment_review, user_id, admin_id }, references)
+
+        return { status: 200, error: undefined, data: store }
+
+    }
+    async update({ request }) {
+
+        const { body, params } = request
+        const { id } = params
+        const { store_name, detail, comment_review, user_id, admin_id } = body
+
+
+        const storeId = await Database
+            .table('stores')
+            .where({ store_id: id })
+            .update({ store_name, detail, comment_review, user_id, admin_id })
+
+        const store = await Database
+            .table('stores')
+            .where({ store_id: storeId })
+            .first()
+
+        return { status: 200, error: undefined, data: store }
+    }
+    async destroy({ request }) {
+        const { id } = request.params
+
+        await Database
+            .table('stores')
+            .where({ store_id: id })
+            .delete()
+
+        return { status: 200, error: undefined, data: { maessage: 'success' } }
+    }
+}
+
+module.exports = StoreController
