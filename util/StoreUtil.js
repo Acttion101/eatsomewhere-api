f7501b6cd6c62a5c3bd92e8162e510ced2754a72
ramita-kahapana <ramitakahapana@gmail.com>
2020-09-13 18:59:39 +0700

    edit-store/util

--- util/StoreUtil.js
+++ util/StoreUtil.js
@@ -4,29 +4,42 @@ class StoreUtil {
         this._Store = StoreModel
     }
 
-    getAll(references) {
-        const store = this._Store.query()
-        if (references) {
-            const extractedReferences = references.split(",");
-            extractedReferences.forEach(reference => {
-                store.with(reference)
 
-            });
-        }
-        return store.fetch()
+    getAll(references) {
+        const stores = this._Store.query()
+
+        return this._withReference(stores, references)
+            .fetch()
     }
 
     getById(storeId, references) {
-        const store = this._Store
+        const stores = this._Store
             .query()
             .where('store_id', storeId)
-        if (references) {
-            const extractedReferences = references.split(",");
-            store.with(extractedReferences)
-        }
-        return store
+
+        return this._withReference(stores, references)
             .fetch()
             .then(response => response.first())
+    }
+
+    async create(userInstance, references) {
+        const { store_id } = await this._Store.create(userInstance)
+        const stores = this._Store
+            .query()
+            .where('store_id', store_id)
+
+        return this._withReference(stores, references)
+            .fetch()
+            .then(response => response.first())
+    }
+
+    _withReference(instance, references) {
+        if (references) {
+            const extractedReferences = references.split(",")
+            instance.with(extractedReferences)
+        }
+
+        return instance
     }
 
 
