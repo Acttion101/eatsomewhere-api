9cc52b09fffc55cf7c76c45f70466678e5861855
Acttion101 <pongponpan102@gmail.com>
2020-09-13 19:24:57 +0700

    edit-factory(seed)

--- database/factory.js
+++ database/factory.js
@@ -1,16 +1,4 @@
 'use strict'
-
-/*
-|--------------------------------------------------------------------------
-| Factory
-|--------------------------------------------------------------------------
-|
-| Factories are used to define blueprints for database tables or Lucid
-| models. Later you can use these blueprints to seed your database
-| with dummy data.
-|
-*/
-
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use('Factory')
 
@@ -27,19 +15,45 @@ Factory.blueprint('App/Models/Admin', (faker) => {
 })
 
 Factory.blueprint('App/Models/User', (faker) => {
-    return {
+     return {
         first_name: faker.first(),
-        last_name: faker.last(),
-        age: faker.age(),
-        user_name: faker.word(),
+         last_name: faker.last(), 
+         age: faker.age(),
+        username: faker.word(),
         password: faker.word({ length: 8 }),
         status: faker.word()
     }
-})
+ })
 
-Factory.blueprint('App/Models/news', (faker) => {
+Factory.blueprint('App/Models/AdminUpdateNew', (faker) => {
     return {
-        news: faker.word({ syllables: 5 }),
-        detail: faker.paragraph({ sentences: 3 })
+        news: faker.sentence({ words: 4 }),
+        detail: faker.sentence()
     }
 })
+
+//  Factory.blueprint('App/Models/Community', (faker) => {
+//      return {
+//          post: faker.sentence({ words: 5 }),
+//          comment_post: faker.sentence()
+         
+//      }
+//  })
+
+//   Factory.blueprint('App/Models/Store', (faker) => {
+//      return {         
+//          store_name: faker.name(),
+//          detail: faker.sentence(),
+//         comment_review: faker.sentence(),
+//       }
+//   })
+
+// Factory.blueprint('App/Models/News', (faker) => {
+//     return {
+//         admin_update_news: faker.sentence({ words: 4 }),
+        
+//     }
+// })
+
+
+
