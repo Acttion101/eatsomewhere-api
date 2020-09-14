49ffa7c77d7a327398f23745379c5f99c96a4fd7
ramita-kahapana <ramitakahapana@gmail.com>
2020-09-15 02:03:22 +0700

    edit-updateutil/test

--- test/unit/updatenew-util.spec.js
+++ test/unit/updatenew-util.spec.js
@@ -1,12 +1,25 @@
 'use strict'
 
-const { test } = use('Test/Suite')('New Util')
+const { test, trait } = use('Test/Suite')('New Util')
 const AdminUpdateUtil = require("../../util/AdminUpdateUtil")
-const MockUpdateNewModel = use("App/Models/AdminUpdateNew")
+const UpdateNew = use("App/Models/AdminUpdateNew")
+const urlUpdateNew = ('/api/v2/admin_update_news')
+trait('Test/ApiClient')
 
+test('get list of new', async({ client }) => {
+    await UpdateNew.create({
+        news: 'ddeee',
+        detail: 'ddddddddddddddddddd',
+
+    })
+
+    const response = await client.get(`${urlUpdateNew}`).end()
+    response.assertStatus(200)
+
+})
 
 test("should get all  news", async({ assert }) => {
-    const updateNewUtil = new AdminUpdateUtil(MockUpdateNewModel);
+    const updateNewUtil = new AdminUpdateUtil(UpdateNew);
     const update_news = await updateNewUtil.getAll()
     assert.isObject(update_news)
 })
