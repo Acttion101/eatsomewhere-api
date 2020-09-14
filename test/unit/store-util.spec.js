cfbe53f1aa251edc42355bfb5b807637027ee390
ramita-kahapana <ramitakahapana@gmail.com>
2020-09-15 02:02:07 +0700

    edit-storeutil/test

--- test/unit/store-util.spec.js
+++ test/unit/store-util.spec.js
@@ -1,13 +1,27 @@
 'use strict'
 
-const { test } = use('Test/Suite')('Store Util')
-
+const { test, trait } = use('Test/Suite')('Store Util')
 const StoreUtil = require("../../util/StoreUtil")
-const MockStoreModel = use("App/Models/Store")
+const urlstore = ('/api/v2/stores')
+trait('Test/ApiClient')
+const Store = use('App/Models/Store')
 
+test('get list of store', async({ client }) => {
+    await Store.create({
+        store_name: 'ddeee',
+        detail: 'ddddddddddddddddddd',
+        comment_review: 'Blog post content',
+        admin_id: '1',
+        user_id: '1'
+    })
+
+    const response = await client.get(`${urlstore}`).end()
+    response.assertStatus(200)
+
+})
 
 test("should get all stores", async({ assert }) => {
-    const storeUtil = new StoreUtil(MockStoreModel);
+    const storeUtil = new StoreUtil(Store);
     const stores = await storeUtil.getAll()
     assert.isObject(stores)
 })
