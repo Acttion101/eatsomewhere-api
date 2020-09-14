2006f6fd8394fa495bfa03c9a27ad2006b924515
Acttion101 <pongponpan102@gmail.com>
2020-09-15 04:37:34 +0700

    edit-insert-test

--- test/unit/insert-test.spec.js
+++ test/unit/insert-test.spec.js
@@ -14,6 +14,9 @@ const CommunityModel = use('App/Models/Community')
 const urlNews = "/api/v2/news"
 const NewsModel = use('App/Models/News')
 
+const urlStore = "/api/v2/stores"
+const StoreModel = use('App/Models/Store')
+
 trait("Test/ApiClient");
 
 test('should insert value to user table', async ({ client }) => {
@@ -52,6 +55,15 @@ test('should insert value to community table', async ({ client }) => {
   test('should insert value to news table', async ({ client }) => {
       const user = {
             update_news_id:"1"
+      }
+      const response = await client.post(urlUsers).send(user).end()
+      response.assertStatus(200)
+    })
+    test('should insert value to store table', async ({ client }) => {
+      const user = {
+            store_name: "actshop",
+            detail: "lowpice",
+            comment_review: "good"
       }
       const response = await client.post(urlUsers).send(user).end()
       response.assertStatus(200)
